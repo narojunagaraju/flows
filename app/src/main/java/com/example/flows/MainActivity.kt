7e87f00ef25de880c3a0985a5e6932f1867383b6
narojunagaraju <narojunagarajun@gmail.com>
2024-04-27 13:17:48 +0530

    flow creation

--- app/src/main/java/com/example/flows/MainActivity.kt
+++ app/src/main/java/com/example/flows/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flows
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,45 +9,55 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.flows.ui.theme.FlowsTheme
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 /***
  * Channels are hot flows, means they emit data even if there are no consumers. So, it will cause loss of data.
  * Flows are mostly cold, they can't emit the data until there are consumers.
  */
-val channel = Channel<Int>()
+
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FlowsTheme {
-                // A surface container using the 'background' color from the theme
-                producer()
-                consumer()
+                LaunchedEffect(key1 = Unit) {
+
+                    GlobalScope.launch {
+                        val data: Flow<Int> = producer()
+                        data.collect {
+                            Log.e(TAG, "onCreate: $it")
+                        }
+                    }
+                }
             }
+        }
+    }
+
+    private fun producer() = flow<Int> { // suspend block, these flows internally creates a coroutine scope
+        val list = listOf(1, 2, 3, 4, 5, 6)
+        list.forEach {
+            delay(1000)
+            emit(it)
         }
     }
 }
 
-fun producer() {
-    CoroutineScope(Dispatchers.Main).launch {
-        channel.send(1)
-        channel.send(2)
-    }
-}
-
-fun consumer() {
-    CoroutineScope(Dispatchers.Main).launch {
-        channel.receive()
-        channel.receive()
-    }
-}
 
